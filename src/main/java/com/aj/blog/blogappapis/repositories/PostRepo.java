823e4115d29e5490a7db285d9e7f82b0ac0fbbe0
theajdev <theajdev@gmail.com>
2025-04-17 11:04:45 +0530

    SearchByTitle added with native query.

--- src/main/java/com/aj/blog/blogappapis/repositories/PostRepo.java
+++ src/main/java/com/aj/blog/blogappapis/repositories/PostRepo.java
@@ -1,6 +1,9 @@
 package com.aj.blog.blogappapis.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import com.aj.blog.blogappapis.entities.Category;
 import com.aj.blog.blogappapis.entities.Post;
@@ -12,5 +15,6 @@ public interface PostRepo extends JpaRepository<Post, Integer> {
 
     List<Post> findByCategory(Category category);
     
-    List<Post> findByTitleContaining(String title);
+    @Query(value="select p from post where p.title like ?1",nativeQuery = true)
+    List<Post> searchByTitle(@Param("key") String title);
 }
