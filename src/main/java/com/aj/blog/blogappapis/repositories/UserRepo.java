cdb5db796aafb026eec156e1bdb0185d1f704189
theajdev <theajdev@gmail.com>
2025-04-17 11:20:47 +0530

    findByEmail method added.

--- src/main/java/com/aj/blog/blogappapis/repositories/UserRepo.java
+++ src/main/java/com/aj/blog/blogappapis/repositories/UserRepo.java
@@ -1,9 +1,11 @@
 package com.aj.blog.blogappapis.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aj.blog.blogappapis.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
-
+	Optional<User> findByEmail(String email);
 }
